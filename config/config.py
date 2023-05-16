582733d7ba208ba1b69bf0bb293139d7b2277a26
XdRonny <127653254+XdRonny@users.noreply.github.com>
2023-05-17 01:40:48 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -54,7 +54,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamYukki/YukkiMusicBot",
+    "https://github.com/XdRonny/sumitherokumusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
